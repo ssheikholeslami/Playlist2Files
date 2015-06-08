46372b974d83873e0214ca8cebfb7b1ddd84334c
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-06-08 18:31:51 +0430

    + Ordered destination file names

--- app/src/main/java/ir/sinash/playlist2files/MainActivity.java
+++ app/src/main/java/ir/sinash/playlist2files/MainActivity.java
@@ -1,6 +1,7 @@
 package ir.sinash.playlist2files;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,9 +16,13 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,6 +31,7 @@ public class MainActivity extends ActionBarActivity {
 
     public Button btnOpenPlaylist;
     private ArrayList<String> listOfFilePaths;
+    private String destFolderPath;
 
 
     @Override
@@ -94,7 +100,7 @@ public class MainActivity extends ActionBarActivity {
 
             long totalSizeInKBs = totalSize/1024;
 
-            Toast.makeText(getApplicationContext(), "Total size: " + totalSizeInKBs, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getApplicationContext(), "Total size: " + totalSizeInKBs, Toast.LENGTH_SHORT).show();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -103,6 +109,68 @@ public class MainActivity extends ActionBarActivity {
         }
 
 
+        //copy file
+
+
+
+        if(isExternalStorageWritable()){
+
+            destFolderPath = Environment.getExternalStorageDirectory().getPath();
+            destFolderPath += "/Playlist2Files/";
+//            Toast.makeText(getApplicationContext(), destFolderPath, Toast.LENGTH_SHORT).show();
+
+            for (int index = 0; index<listOfFilePaths.size(); index++){
+
+                String sourcePath = listOfFilePaths.get(index);
+                String sourceFileName = "";
+
+                StringTokenizer tokenizer = new StringTokenizer(sourcePath, "/");
+
+                //int numOfTokens = tokenizer.countTokens();
+
+
+                while(tokenizer.hasMoreTokens())
+                {
+                    sourceFileName = tokenizer.nextToken();
+                }
+
+
+                /**
+                for (String token : sourcePath.split("/")) {
+
+                    System.err.println(token);
+                }
+                */
+                String destPath = destFolderPath+"00"+String.valueOf(index+1)+" - "+sourceFileName;
+                Toast.makeText(getApplicationContext(), destPath, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
     }
 
     @Override
